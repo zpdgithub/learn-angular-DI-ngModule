f2866adcda5a53dfcd83116812732ae759c3a014
peter <peter@soez.io>
2018-04-06 13:20:16 +0800

    创建AppService，并注入AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
 
+class AppService {
+  get(): void {
+    console.log('get AppService');
+  }
+}
+
 @Component({
   selector: 'app-root',
   template: `root`
 })
 export class AppComponent {
+  constructor(private appService: AppService) {
+    this.appService.get();
+  }
 }
 
 @NgModule({
@@ -16,7 +25,9 @@ export class AppComponent {
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    AppService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
